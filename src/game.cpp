4cfdc33493c30bc593e4a7ac4fe45288895cf07f
Przemek Badaruk <przemekkth@outlook.com>
2022-07-13 21:26:56 +0200

    Create constants

--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,49 @@
 #include "game.h"
 
+const QSize Game::RESOLUTION = QSize(640, 480);
+const unsigned int Game::FPS = 30;
+const unsigned int Game::REVEAL_SPEED = 8;
+const unsigned int Game::BOX_SIZE = 40;
+const unsigned int Game::GAP_SIZE = 10;
+const unsigned int Game::BOARD_WIDTH = 10;
+const unsigned int Game::BOARD_HEIGHT = 7;
+
+const int Game::X_MARGIN = int( (RESOLUTION.width() - (BOARD_WIDTH * (BOX_SIZE + GAP_SIZE) )) / 2);
+const int Game::Y_MARGIN = int( (RESOLUTION.height() - (BOARD_HEIGHT * (BOX_SIZE + GAP_SIZE) )) / 2);;
+
+//                                            R    G    B
+const QColor Game::GRAY_COLOR      = QColor(100, 100, 100);
+const QColor Game::NAVYBLUE_COLOR  = QColor( 60,  60, 100);
+const QColor Game::WHITE_COLOR     = QColor(255, 255, 255);
+const QColor Game::RED_COLOR       = QColor(255,   0,   0);
+const QColor Game::GREEN_COLOR     = QColor(  0, 255,   0);
+const QColor Game::BLUE_COLOR      = QColor(  0,   0, 255);
+const QColor Game::YELLOW_COLOR    = QColor(255, 255,   0);
+const QColor Game::ORANGE_COLOR    = QColor(255, 128,   0);
+const QColor Game::PURPLE_COLOR    = QColor(255,   0, 255);
+const QColor Game::CYAN_COLOR      = QColor(  0, 255, 255);
+
+const QColor Game::BG_COLOR = NAVYBLUE_COLOR;
+const QColor Game::LIGHTBG_COLOR = GRAY_COLOR;
+const QColor Game::BOX_COLOR = WHITE_COLOR;
+const QColor Game::HIGHLIGHT_COLOR = BLUE_COLOR;
+
+const QString Game::DONUT = "donut";
+const QString Game::SQUARE = "square";
+const QString Game::DIAMOND = "diamond";
+const QString Game::LINES = "lines";
+const QString Game::OVAL = "oval";
+
+QList<QColor> Game::ALL_COLORS = {
+    GRAY_COLOR, NAVYBLUE_COLOR, WHITE_COLOR, RED_COLOR,
+    GREEN_COLOR, BLUE_COLOR, YELLOW_COLOR, ORANGE_COLOR,
+    PURPLE_COLOR, CYAN_COLOR
+};
+QStringList   Game::ALL_SHAPES = {
+    DONUT, SQUARE, DIAMOND, LINES, OVAL
+};
+
 Game::Game()
 {
-
+    Q_STATIC_ASSERT_X((BOARD_WIDTH * BOARD_HEIGHT) % 2 == 0, "Board needs to have an even number of boxes for pairs of matches.");
 }
--- src/game.h
+++ src/game.h
@@ -1,11 +1,48 @@
 #ifndef GAME_H
 #define GAME_H
-
+#include <QSize>
+#include <QColor>
+#include <QList>
 
 class Game
 {
 public:
     Game();
+    static const QSize RESOLUTION;
+    static const unsigned int FPS;
+    static const unsigned int REVEAL_SPEED;
+    static const unsigned int BOX_SIZE;
+    static const unsigned int GAP_SIZE;
+    static const unsigned int BOARD_WIDTH;
+    static const unsigned int BOARD_HEIGHT;
+
+    static const int X_MARGIN;
+    static const int Y_MARGIN;
+
+    static const QColor GRAY_COLOR;
+    static const QColor NAVYBLUE_COLOR;
+    static const QColor WHITE_COLOR;
+    static const QColor RED_COLOR;
+    static const QColor GREEN_COLOR;
+    static const QColor BLUE_COLOR;
+    static const QColor YELLOW_COLOR;
+    static const QColor ORANGE_COLOR;
+    static const QColor PURPLE_COLOR;
+    static const QColor CYAN_COLOR;
+
+    static const QColor BG_COLOR;
+    static const QColor LIGHTBG_COLOR;
+    static const QColor BOX_COLOR;
+    static const QColor HIGHLIGHT_COLOR;
+
+    static const QString DONUT;
+    static const QString SQUARE;
+    static const QString DIAMOND;
+    static const QString LINES;
+    static const QString OVAL;
+
+    static QList<QColor> ALL_COLORS;
+    static QStringList   ALL_SHAPES;
 };
 
 #endif // GAME_H
